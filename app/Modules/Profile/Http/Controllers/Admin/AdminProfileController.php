3f617a56ca6ab20709be2ef95a885511570c1415
Peter Joshua Pangilinan Brion <exequias1022@gmail.com>
2026-02-03 18:20:54 +0800

    Add search capability on Profile module.

--- app/Modules/Profile/Http/Controllers/Admin/AdminProfileController.php
+++ app/Modules/Profile/Http/Controllers/Admin/AdminProfileController.php
@@ -8,9 +8,21 @@ use Illuminate\Http\Request;
 
 class AdminProfileController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return User::with('profile')->paginate(20);
+        $query = User::with('profile');
+
+        // --- Search by user fields ---
+        if ($request->filled('q')) {
+            $q = $request->q;
+
+            $query->where(function ($builder) use ($q) {
+                $builder->where('name', 'like', "%{$q}%")
+                    ->orWhere('email', 'like', "%{$q}%");
+            });
+        }
+
+        return $query->paginate(20);
     }
 
     public function show(User $user)
