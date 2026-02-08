43f76a595528765ac14328c916231443444ff5ea
Peter Joshua Pangilinan Brion <exequias1022@gmail.com>
2026-02-08 13:59:21 +0800

    WIP: Dashboard functionality

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 
+Route::prefix('api')->middleware(["auth:api", "role:ADMIN|MODERATOR"])->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index']);
+    });
+});
