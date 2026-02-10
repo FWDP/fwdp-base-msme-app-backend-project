4441e107bd26202372aa52a7b84b57fcd89d8600
Peter Joshua Pangilinan Brion <exequias1022@gmail.com>
2026-02-10 21:33:00 +0800

    Making Subscriptions core module protected.

--- app/Core/Subscriptions/Providers/SubscriptionsServiceProvider.php
+++ app/Core/Subscriptions/Providers/SubscriptionsServiceProvider.php
@@ -9,13 +9,6 @@ class SubscriptionsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        if (!DB::table('modules')
-            ->where('name', 'core_subscriptions')
-            ->where('enabled', true)
-            ->exists()) {
-            return;
-        }
-
         $this->loadRoutesFrom(base_path()."/app/Core/Subscriptions/routes/api.php");
         $this->loadMigrationsFrom(base_path()."/app/Core/Subscriptions/database/migrations");
     }
